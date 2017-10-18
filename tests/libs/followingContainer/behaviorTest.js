45d423bfbbdef9f4250c07dc4e8c7408c8ae56c5
sdarmaputra <surya.igede@gmail.com>
2017-10-18 18:37:41 +0700

    Test suite finished and add .travis.yml - removing unnecessary dependencies - finishing behaviorTest - add .travis.yml

--- tests/libs/followingContainer/behaviorTest.js
+++ tests/libs/followingContainer/behaviorTest.js
@@ -38,7 +38,109 @@ const behaviorTest = () => {
 					clearTimeout(wait);
 				}, 5000);
 			});
+		});
+	
+		describe('related to lowerStopPoint option', function() {
+			beforeEach(function() {
+				this.upperStopPoint = 200;
+				this.lowerStopPoint = 1000;
+				this.component = renderComponent({ upperStopPoint: this.upperStopPoint, lowerStopPoint: this.lowerStopPoint });
+			});
 
+			it('should change its top position if window scrolled between upperStopPoint and lowerStopPoint', function() {
+				let nextYPosition = 1999;
+				let currentConfigs = this.component.wrapper.instance().currentConfigs;
+				let oldYPosition = this.component.getLatestComputedStyle().top;
+				let expectedYPosition = nextYPosition + currentConfigs.viewportPaddingTop;
+				window.scrollTo(0, nextYPosition);
+				let wait = setTimeout(function() {
+					expect(this.component.getLatestComputedStyle().top).to.not.equal(`${oldYPosition}px`);
+					expect(this.component.getLatestComputedStyle().top).to.equal(`${expectedYPosition}px`);
+					clearTimeout(wait);
+				}, 5000);
+			});
+
+			it('should not change its top position beyond lowerStopPoint', function() {
+				let nextYPosition = 2100;
+				let currentConfigs = this.component.wrapper.instance().currentConfigs;
+				let oldYPosition = this.component.getLatestComputedStyle().top;
+				window.scrollTo(0, nextYPosition);
+				let wait = setTimeout(function() {
+					expect(this.component.getLatestComputedStyle().top).to.equal(`${oldYPosition}px`);
+					clearTimeout(wait);
+				}, 5000);
+			});
+		});
+
+		describe('related to hideOnStart option', function() {
+			beforeEach(function(done) {
+				this.upperStopPoint = 200;
+				this.lowerStopPoint = 200;
+				this.hideOnStart = true;
+				window.scrollTo(0, 0);
+				let wait = setTimeout(function() {
+					this.component = renderComponent({
+						upperStopPoint: this.upperStopPoint,
+						lowerStopPoint: this.lowerStopPoint,
+						hideOnStart: this.hideOnStart
+					});
+					clearTimeout(wait);
+					done();
+				}, 1500);
+			});	
+
+			it('should have zero opacity after initial render', function() {
+				window.scrollTo(0, 0);
+				let wait = setTimeout(function() {
+					expect(this.component.getLatestComputedStyle().opacity).to.equal(0);
+					clearTimeout(wait);
+				}, 5000);
+			});
+
+			it('should have zero opacity when window scroll below upperStopPoint', function() {
+				let nextYPosition = 100;
+				window.scrollTo(0, nextYPosition);
+				let wait = setTimeout(function() {
+					expect(this.component.getLatestComputedStyle().opacity).to.equal(0);
+				});
+			});
+				
+			it('should have full opacity when window scroll beyond upperStopPoint', function() {
+				let nextYPosition = 210;
+				window.scrollTo(0, nextYPosition);
+				let wait = setTimeout(function() {
+					expect(this.component.getLatestComputedStyle().opacity).to.equal(1);
+				});
+			});	
+		});
+			
+		describe('related to hideOnTop option', function() {
+			beforeEach(function() {
+				this.upperStopPoint = 200;
+				this.lowerStopPoint = 200;
+				this.hideOnTop = true;
+				this.component = renderComponent({
+					upperStopPoint: this.upperStopPoint,
+					lowerStopPoint: this.lowerStopPoint,
+					hideOnTop: this.hideOnTop
+				});
+			});	
+
+			it('should have full opacity when window scrolled beyond upperStopPoint', function() {
+				window.scrollTo(0, 205);
+				let wait = setTimeout(function() {
+					expect(this.component.getLatestComputedStyle().opacity).to.equal(1);
+					clearTimeout(wait);
+				}, 5000);
+			});
+
+			it('should have zero opacity when window scrolled below upperStopPoint', function() {
+				let nextYPosition = 100;
+				window.scrollTo(0, nextYPosition);
+				let wait = setTimeout(function() {
+					expect(this.component.getLatestComputedStyle().opacity).to.equal(0);
+				});
+			});		
 		});
 	});
 }
